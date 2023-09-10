15b78156f969271d3526089e4f565c23bcf2ab07
Eduardo Fernandes <129226280+eduardokenzinho@users.noreply.github.com>
2023-09-09 21:27:38 -0300

    primeiro commit

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 /* Desenvolva seu código abaixo */
+    let Pedra = "pedra"
+    let Papel = "papel"
+    let Tesoura = "tesoura"
+
+function playRockPaperScissor(player1, player2){
+    if(player1 == "pedra" && player2 == "papel"){
+        return 'Jogador 2 venceu!'
+    }else if(player1 == "papel" && player2 == "pedra"){
+        return 'Jogador 1 venceu!'
+    }else if(player1 == "pedra" && player2 == "pedra"){
+        return 'Empate!'
+    }else if(player1 == "papel" && player2 == "papel"){
+        return 'Empate!'
+    }else if(player1 == "tesoura" && player2 == "papel"){
+        return 'Jogador 1 venceu!'
+    }else if(player1 == "papel" && player2 == "tesoura"){
+        return 'Jogador 2 venceu!'
+    }else if(player1 == "papel" && player2 == "papel"){
+        return 'Empate!'
+    }else if(player1 == "tesoura" && player2 == "tesoura"){
+        return 'Empate!'
+    }else if(player1 == "tesoura" && player2 == "pedra"){
+        return 'Jogador 2 venceu!'
+    }else if(player1 == "pedra" && player2 == "tesoura"){
+        return 'Jogador 1 venceu!'
+    }
+}
+console.log (playRockPaperScissor);
