d9bd9bc0a2cbf2ff954350d45e4e4562563a721c
Eduardo Fernandes <129226280+eduardokenzinho@users.noreply.github.com>
2023-09-13 12:44:07 -0300

    corrigindo de acordo com a rubrica

--- script.js
+++ script.js
@@ -1,27 +1,16 @@
 /* Desenvolva seu código abaixo */
+    
+
+function playRockPaperScissor(player1, player2){
     let Pedra = "Pedra"
     let Papel = "Papel"
     let Tesoura = "Tesoura"
 
-function playRockPaperScissor(player1, player2){
-    if(player1 == Pedra && player2 == Papel){
+    if(player1 == Pedra && player2 == Papel || player1 == Papel && player2 == Tesoura || player1 == Tesoura && player2 == Pedra){
         return 'Jogador 2 venceu!'
-    }else if(player1 == Papel && player2 == Pedra){
+    }else if(player1 == Papel && player2 == Pedra || player1 == Tesoura && player2 == Papel || player1 == Pedra && player2 == Tesoura){
         return 'Jogador 1 venceu!'
-    }else if(player1 == Pedra && player2 == Pedra){
+    }else if(player1 == Pedra && player2 == Pedra || player1 == Papel && player2 == Papel || player1 == Tesoura && player2 == Tesoura){
         return 'Empate!'
-    }else if(player1 == Papel && player2 == Papel){
-        return 'Empate!'
-    }else if(player1 == Tesoura && player2 == Papel){
-        return 'Jogador 1 venceu!'
-    }else if(player1 == Papel && player2 == Tesoura){
-        return 'Jogador 2 venceu!'
-    }else if(player1 == Tesoura && player2 == Tesoura){
-        return 'Empate!'
-    }else if(player1 == Tesoura && player2 == Pedra){
-        return 'Jogador 2 venceu!'
-    }else if(player1 == Pedra && player2 == Tesoura){
-        return 'Jogador 1 venceu!'
-    }
-}
+    }}
 console.log (playRockPaperScissor);
